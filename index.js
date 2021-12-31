222146091fd25de53ffef712a937961b3d5dac12
Sunil <snilavarath@gmail.com>
2021-12-31 12:55:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 /* Your Code Here */
+const createEmployeeRecord =(array)=>{
+    return{
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    } 
+}
 
+const createEmployeeRecords = (employeeArr)=>{
+    return employeeArr.map(rec=>createEmployeeRecord(rec))     
+} 
+
+const createTimeInEvent = function (stamp){
+    let timeIn= {
+        type:"TimeIn",
+        hour: parseInt(stamp.split(' ')[1]),
+        date: stamp.split(' ')[0]
+    }
+    this.timeInEvents.push(timeIn)
+    return this    
+}
+
+const createTimeOutEvent= function(stamp){ 
+    let timeOut= {
+        type:"TimeOut",
+        hour: parseInt(stamp.split(' ')[1]),
+        date: stamp.split(' ')[0]
+    }
+    this.timeOutEvents.push(timeOut)
+    return this    
+}
+
+
+const hoursWorkedOnDate= function(string){ 
+    let timeIn = this.timeInEvents.find(event=>{
+        return event.date===string
+    }).hour
+    let timeOut = this.timeOutEvents.find(event=>{
+        return event.date===string
+    }).hour
+    let hours= (timeOut-timeIn)/100
+    return hours        
+}
+
+const wagesEarnedOnDate= function(date){
+    return hoursWorkedOnDate.call(this, date)*this.payPerHour       
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -8,6 +57,9 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+
+
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +73,13 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function(employeeArr,string){
+    let employee = employeeArr.find(event=>event.firstName === string)
+    return employee
+}
+
+const calculatePayroll = function(array){
+    return array.reduce((total, record)=>{
+        return total + allWagesFor.call(record)
+    }, 0)    
+}
